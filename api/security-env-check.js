8bd80fa13d97be777da93def925d429d993d0305
zakeronline45-cyber <zakeronline45@gmail.com>
2026-09-24 19:32:02 +0300

    Remove temporary security environment endpoint

--- api/security-env-check.js
+++
@@ -1,7 +0,0 @@
-export default async function handler(req,res){
-  if(req.method!=='GET') return res.status(405).json({ok:false});
-  return res.status(200).json({
-    serviceRoleConfigured:!!process.env.SUPABASE_SERVICE_ROLE_KEY,
-    openAIConfigured:!!process.env.OPENAI_API_KEY
-  });
-}
